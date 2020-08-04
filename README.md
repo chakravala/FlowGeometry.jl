7689fc15a9a7f4685e618a3ef92660d3801e63ce
Michael Reed <18372368+chakravala@users.noreply.github.com>
2020-08-04 00:32:30 -0400

    improved performance of NACA profiles

---
+++ README.md
@@ -0,0 +1,17 @@
+# FlowGeometry.jl
+
+*Geometry for computational fluid dynamics flows with [Grassmann.jl](https://github.com/chakravala/Grassmann.jl) elements*
+
+[![Build Status](https://travis-ci.org/chakravala/FlowGeometry.jl.svg?branch=master)](https://travis-ci.org/chakravala/FlowGeometry.jl)
+[![Build status](https://ci.appveyor.com/api/projects/status/b4v4f088pg15pm1u?svg=true)](https://ci.appveyor.com/project/chakravala/flowgeometry-jl)
+[![Coverage Status](https://coveralls.io/repos/chakravala/FlowGeometry.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/chakravala/FlowGeometry.jl?branch=master)
+[![codecov.io](https://codecov.io/github/chakravala/FlowGeometry.jl/coverage.svg?branch=master)](https://codecov.io/github/chakravala/FlowGeometry.jl?branch=master)
+
+## References
+* C. H. Zimmerman, [Characteristics of Clark Y airfoils of small aspect ratios](http://naca.central.cranfield.ac.uk/reports/1933/naca-report-431.pdf), NACA (1932)
+* Eastman N. Jacobs, Kenneth E. Ward, and Robert M. Pinkerton, [The characteristics of 78 related airfoil sections from tests in the variable-density wind tunnel](https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19930091108.pdf), NACA (1933)
+* John Stack and Albert E. von Doenhoff, [Tests of 16 related airfoils at high speeds](http://naca.central.cranfield.ac.uk/reports/1935/naca-report-492.pdf), NACA (1934)
+* Eastman N. Jacobs and Robert M. Pinkerton, [Tests in the variable-density wind tunnel of related airfoils having the maximum camber unusually far forward](https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19930091610.pdf), NACA (1935)
+* Laurence K. Loftin, [Theoretical and experimental data for a number of NACA 6A-series airfoil sections](http://www.pdas.com/refs/rep903.pdf), NACA (1947)
+* Charles L. Ladson, Cuyler W. Brooks, Acquilla S. Hill, and Darrel W. Sproles, [Computer program to obtain ordinates for NACA airfiols](https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19970008124.pdf), NASA (1996)
+* W. H. Mason, [Geometry for aerodynamicists](http://www.dept.aoe.vt.edu/~mason/Mason_f/ConfigAeroAppA.pdf) (2006)
--- src/profiles.jl
+++ src/profiles.jl
@@ -11,10 +11,10 @@ export profile, profileslope, interval
 
 abstract type Profile{P} end
 
-@pure profile(p::T,x,c,x0=0) where T<:Profile = profile(p,(x-x0)/c)*c
-@pure profileslope(p::T,x,c,x0=0) where T<:Profile = profileslope(p,(x-x0)/c)
 profile(p::T) where T<:Profile = profile.(Ref(p),interval(p))
 profileslope(p::T) where T<:Profile = profileslope.(Ref(p),interval(p))
+@pure profile(p::T,x,c,x0=0) where T<:Profile = profile(p,(x-x0)/c)*c
+@pure profileslope(p::T,x,c,x0=0) where T<:Profile = profileslope(p,(x-x0)/c)
 @pure interval(::P,c=1,x0=0) where P<:Profile{p} where p = interval(p,c,x0)
 @pure interval(p::Int,c=1,x0=0) = range(x0,c,length=p)
 function points(N::A,c=1,x0=0) where A<:Profile{p} where p
@@ -66,31 +66,35 @@ end
 profileslope(P::CircularArc,x::Chain) = profileslope(P,x[2])
 profileslope(P::CircularArc,x::Chain,c,x0=0) = profileslope(P,x[2],c,x0)
 
-# NACA 4-series thickness
+# NACA Clark Y
+# http://naca.central.cranfield.ac.uk/reports/1933/naca-report-431.pdf
 
 struct ClarkY{t,p} <: Profile{p}
     @pure ClarkY{t,p}() where {t,p} = new{t,p}()
 end
 
-@pure Y(x) = SVector(sqrt(x),x,x^2,x^3,x^4)
-@pure dY(x) = SVector(0.5/sqrt(x),1.0,2x,3x^2,4x^3)
-const _clarky = SVector(0.2969,-0.1260,-0.3515,0.2843,-0.1036)
+const ℝ5 = SubManifold(ℝ^5)
+
+@pure Y(x) = Chain{ℝ5,1}(sqrt(x),x,x^2,x^3,x^4)
+@pure dY(x) = Chain{ℝ5,1}(0.5/sqrt(x),1.0,2x,3x^2,4x^3)
+const _clarky = Chain{ℝ5,1}(0.2969,-0.1260,-0.3515,0.2843,-0.1036)
 
 @pure function thickness(x,a,t)
-    (x<0 || x>1) ? 0.0 : 5t*(Y(x)⋅a)
+    (x<0 || x>1) ? 0.0 : 5t*(Y(x)⋅a)[1]
 end # thickness distribution
 @pure function thickness_slope(x,a,t)
-    (x<0 || x>1) ? 0.0 : 5t*(dY(x)⋅a)
+    (x<0 || x>1) ? 0.0 : 5t*(dY(x)⋅a)[1]
 end # thickness distribution slope
 
 @pure profile(::ClarkY{t},x) where t = thickness(x,_clarky,t/100)
 @pure profileslope(::ClarkY{t},x) where t = thickness_slope(x,_clarky,t/100)
 
-# NACA custom thickness
+# NACA 4-series thickness
+# https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19930091108.pdf
 
 struct Thickness{t,x,te,p} <: Profile{p}
     @pure Thickness{t,x,te,p}() where {t,x,te,p} = new{t,x,te,p}()
-    @pure Thickness{t,x,p}() where {t,x,p} = Thickness{t,x,0.2,p}()
+    @pure Thickness{t,x,p}() where {t,x,p} = Thickness{t,x,t/100,p}()
     @pure Thickness{t,p}() where {t,p} = Thickness{t,3,p}()
 end
 
@@ -99,68 +103,53 @@ end
 @pure riegel(x) = -(2.24-5.42x+12.3x^2)/(10(1-0.878x))
 
 @pure function clarky(x=0.3,te=0.002,ts=tailslope(x),n=0.1,y=0.078)
-    SMatrix{5,5}(Y(x)...,dY(x)...,Y(1)...,dY(1)...,Y(n)...)'\SVector(0.1,0,te,ts,y)
+    transpose(Chain{ℝ5,1}(Y(x),dY(x),Y(1.0),dY(1.0),Y(n)))\Chain{ℝ5,1}(0.1,0.0,te,ts,y)
 end
 
 @pure @generated profile(::Thickness{t,m,te},x) where {t,m,te} = :(thickness(x,$(clarky(m/10,te/100)),$(t/100)))
 @pure @generated profileslope(::Thickness{t,m,te},x) where {t,m,te} = :(thickness_slope(x,$(clarky(m/10,te/100)),$(t/100)))
 
 # NACA mofified thickness
+# http://naca.central.cranfield.ac.uk/reports/1935/naca-report-492.pdf
 
 struct Modified{t,m,p} <: Profile{p}
     @pure Modified{t,m,p}() where {t,m,p} = new{t,m,p}()
     @pure Modified{t,p}() where {t,p} = new{t,63,p}()
 end
 
-@pure function decode(::Modified{t,m}) where {t,m}
-    i = Int(m÷10)
-    t/100,i≠9 ? i : 6sqrt(3),(m%10)/10
+@pure function modified(::Modified{t,m}) where {t,m}
+    i = Int(m÷10); t/100,i≠9 ? i : 6sqrt(3),(m%10)/10
 end
 
-@pure D(x) = (x1 = 1-x; SVector(1.0,x1,x1^2,x1^3))
-@pure dD(x) =  (x1 = x-1; SVector(0.0,-1.0,2x1,-3x1^2))
-@pure ddD(x) =  SVector(0.0,0.0,2,-6(x-1))
+const ℝ4 = SubManifold(ℝ^4)
 
-@pure A(x) = SVector(sqrt(x),x,x^2,x^3)
-@pure dA(x) =  SVector(0.5/sqrt(x),1.0,2x,3x^2)
-@pure ddA(x) =  SVector(-1/(4x*sqrt(x)),0.0,2,6x)
+@pure D(x) = (x1 = 1-x; Chain{ℝ4,1}(1.0,x1,x1^2,x1^3))
+@pure dD(x) =  (x1 = x-1; Chain{ℝ4,1}(0.0,-1.0,2x1,-3x1^2))
+@pure ddD(x) =  Chain{ℝ4,1}(0.0,0.0,2.0,-6(x-1))
 
-@pure function modified(x,a,p,t=1)
-    (x<0 || x>1) ? 0.0 : 5t*(x < p ? A(x)⋅a[1] : D(x)⋅a[2])
-end # modified thickness
-@pure function modified_slope(x,a,p,t=1)
-    (x<0 || x>1) ? 0.0 : 5t*(x < p ? dA(x)⋅a[1] : dD(x)⋅a[2])
-end # thickness distribution slope
-
-@pure function profile(P::Modified,x)
-    t,i,p = decode(P)
-    modified(x,coef(t,i,p),p,t)
-end
-@pure function profileslope(P::Modified,x)
-    t,i,p = decode(P)
-    modified_slope(x,coef(t,i,p),p,t)
-end
+@pure A(x) = Chain{ℝ4,1}(sqrt(x),x,x^2,x^3)
+@pure dA(x) =  Chain{ℝ4,1}(0.5/sqrt(x),1.0,2x,3x^2)
+@pure ddA(x) =  Chain{ℝ4,1}(-1/(4x*sqrt(x)),0.0,2.0,6x)
 
 @pure radius(t,::Val{a0}=Val(_clarky[1])) where a0 = t^2*((a0/0.2)^2/2)
 
-#transpose(Chain{V,1}(Chain{V,1}(D(x)),Chain{V,1}(dD(x)),Chain{V,1}(D(1)),Chain{V,1}(dD(1))))\Chain{V,1}(0.1,0.0,0.002,-tailangle(x))
-#=function coef(t,i,x,te=0.002,ts=tailslope(x))
-    di = SMatrix{4,4}(D(x)...,dD(x)...,D(1)...,dD(1)...)'\SVector(0.1,0,te,ts)
-    ai = SMatrix{4,4}(A(x)...,dA(x)...,ddA(x)...,1,0,0,0)'\SVector(0.1,0,ddD(x)⋅di,(1/5t)*sqrt(2radius(0.1*i/6)))
-    ai,di
-end=#
-@pure function coef(t,i,x,te=0.002,ts=tailslope(x))
-    d0,d1,x1 = te,-ts,1-x
-    d3 = (3d1 - (0.588/x1)) / 3x1^2
-    d2 = (-1.5x1*d3) - (0.5d1 / x1)
-    a0 = (0.2/t)*sqrt(2radius(t*i/6))
-    a3 = 0.1/x^3 + (d1*x1-0.294)/(x*x1^2) - (3/8)*a0/(x^2.5)
-    a2 = a0/2sqrt(x^3) - 0.1/x^2 - 2a3*x
-    a1 = -(a0/2sqrt(x) + 2a2*x + 3a3*x^2)
-    return SVector(a0,a1,a2,a3),SVector(d0,d1,d2,d3)
+@pure function modified(t,i,x,te=0.002,ts=tailslope(x))
+    d = transpose(Chain{ℝ4,1}(D(x),dD(x),D(1.0),dD(1.0)))\Chain{ℝ4,1}(0.1,0.0,te,ts)
+    a = transpose(Chain{ℝ4,1}(A(x),dA(x),ddA(x),Chain{ℝ4,1}(1.0,1.0,1.0,1.0)))
+    return a\Chain{ℝ4,1}(0.1,0.0,(ddD(x)⋅d)[1],(1/5t)*sqrt(2radius(0.2*i/6))),d
+end
+
+@pure function profile(P::Modified,x)
+    t,i,p = modified(P); a = modified(t,i,p)
+    (x<0 || x>1) ? 0.0 : 5t*(x < p ? A(x)⋅a[1] : D(x)⋅a[2])[1]
+end
+@pure function profileslope(P::Modified,x)
+    t,i,p = modified(P); a = modified(t,i,p)
+    (x<0 || x>1) ? 0.0 : 5t*(x < p ? dA(x)⋅a[1] : dD(x)⋅a[2])[1]
 end
 
 # NACA camber
+# https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19930091610.pdf
 
 struct Camber{n,p} <: Profile{p}
     @pure Camber{n,p}() where {n,p} = new{n,p}()
@@ -181,7 +170,7 @@ end
         approx(p,[-1/250,359/300,7/10,10/3,0]),
         #k: p -> [361.4,51.64,15.957,6.643,3.230]
         approx(p,[284037/200,-3296847/100,4296829/15,-1087744,4544800/3]),0
-    else
+    else # reflexed
         #p: [0.1,0.15,0.2,0.25]
         #m: p -> [0.13,0.217,0.318,0.441]
         approx(p,[17/500,26/15,-2,32/3]),
@@ -193,53 +182,42 @@ end
     return m*k1,r,k1k2
 end
 
-# derivation
+const ℝ3 = SubManifold(ℝ^3)
 
-#=yc(x) = SVector(1,x,x^2)
-dyc(x) = SVector(0,1,2x)
+@pure C(x) = Chain{ℝ3,1}(1.0,x,x^2)
+@pure dC(x) = Chain{ℝ3,1}(0.0,1.0,2x)
 
-@pure function clarkY(m,p)
-    SMatrix{3,3}(yc(0)...,yc(p)...,dyc(p)...)'\SVector(0,m,0),
-    SMatrix{3,3}(yc(1)...,yc(p)...,dyc(p)...)'\SVector(0,m,0)
-end=#
+@pure function camber2(m,p)
+    cm,y,dy = Chain{ℝ3,1}(m,0.0,0.0),C(p),dC(p)
+    transpose(Chain{ℝ3,1}(y,dy,C(0.0)))\cm,
+    transpose(Chain{ℝ3,1}(y,dy,C(1.0)))\cm
+end
 
-@pure function camber2(::Camber{n},x) where n
-    (x<0 || x>1) && return 0.0
-    m,p = camber2(n)
-    return m*(if x < p
-        xp = x/p
-        2xp-xp^2
+@pure function profile(::Camber{n},x) where n
+    (x<0 || x>1) && (return 0.0)
+    if n < 100
+        m,p = camber2(n)
+        (C(x)⋅camber2(m,p)[x < p ? 1 : 2])[1]
     else
-        (1+2p*(x-1)-x^2)/(1-p)^2
-    end)
+        mk1,p,k1k2 = camber3(n)
+        mk1*(p^3*(1-x)+(if x < p
+            (x-p)^3-(k1k2≠0 ? k1k2*x*(1-p)^3 : 0)
+        else
+            k1k2≠0 ? k1k2*((x-p)^3-x*(1-p)^3) : 0
+        end))/6
+    end
 end
-@pure function camber2slope(::Camber{n},x) where n
-    (x<0 || x>1) && return 0.0
-    m,p = camber2(n)
-    2m*(p-x)/(x<p ? p^2 : (1-p)^2)
-end
-@pure function camber3(::Camber{n},x) where n
-    (x<0 || x>1) && 0.0
-    mk1,p,k1k2 = camber3(n)
-    mk1*(p^3*(1-x)+(if x < p
-        #x^3-3p*x^2+p^2*(3-p)*x
-        (x-p)^3-(k1k2≠0 ? k1k2*x*(1-p)^3 : 0)
+@pure function profileslope(::Camber{n},x) where n
+    (x<0 || x>1) && (return 0.0)
+    if n < 100
+        m,p = camber2(n)
+        (dC(x)⋅camber2(m,p)[x < p ? 1 : 2])[1]
     else
-        k1k2≠0 ? k1k2*((x-p)^3-x*(1-p)^3) : 0
-    end))/6
+        mk1,p,k1k2 = camber3(n)
+        mk1*(-p^3+(if x < p
+            3(p-x)^2-(k1k2≠0 ? k1k2*(1-p)^3 : 0)
+        else
+            k1k2≠0 ? 3k1k2*((p-x)^2-(1-p)^3) : 0
+        end))
+    end
 end
-@pure function camber3slope(::Camber{n},x) where n
-    (x<0 || x>1) && 0.0
-    mk1,p,k1k2 = camber3(n)
-    mk1*(-p^3+(if x < p
-        #3x^2-6p*x+p^2*(3-p)
-        3(p-x)^2-(k1k2≠0 ? k1k2*(1-p)^3 : 0)
-    else
-        k1k2≠0 ? 3k1k2*((p-x)^2-(1-p)^3) : 0
-    end))
-end
-
-@pure profile(P::Camber{n},x) where n = n < 100 ? camber2(P,x) : camber3(P,x)
-@pure profileslope(P::Camber{n},x) where n = n < 100 ? camber2slope(P,x) : camber3slope(P,x)
-profile(P::Camber{n}) where n = (x=interval(P); n < 100 ? camber2.(Ref(P),x) : camber3.(Ref(P),x))
-profileslope(P::Camber{n}) where n = (x=interval(P); n < 100 ? camber2slope.(Ref(P),x) : camber3slope.(Ref(P),x))
